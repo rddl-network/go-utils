1eba0ba1bba07f2699e69e2c9018114d4d6fe3a7
Lorenz Herzberger <lorenzherzberger@gmail.com>
2024-05-07 16:46:43 +0200

    feat: add secp256r1 signature verification
    Signed-off-by: Lorenz Herzberger <lorenzherzberger@gmail.com>

--- signature/signature_test.go
+++ signature/signature_test.go
@@ -1,6 +1,10 @@
 package signature_test
 
 import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/sha256"
 	"encoding/hex"
 	"testing"
 
@@ -24,4 +28,33 @@ func TestValidateSignature(t *testing.T) {
 	valid, err := signature.ValidateSignature(hexMsg, hexSign, hexPublicKey)
 	assert.True(t, valid)
 	assert.NoError(t, err)
+}
+
+func TestValidateSECP256R1Signature(t *testing.T) {
+	curve := elliptic.P256()
+	prvKey, err := ecdsa.GenerateKey(curve, rand.Reader)
+	assert.NoError(t, err)
+
+	pubKey := prvKey.PublicKey
+
+	digest := []byte("msg")
+	hash := sha256.Sum256(digest)
+	r, s, err := ecdsa.Sign(rand.Reader, prvKey, hash[:])
+	assert.NoError(t, err)
+
+	var signBytes []byte
+	signBytes = append(signBytes, r.Bytes()...)
+	signBytes = append(signBytes, s.Bytes()...)
+
+	hexDigest := hex.EncodeToString(digest)
+	hexSign := hex.EncodeToString(signBytes)
+	hexPubKey := hex.EncodeToString(signature.PublicKeyToUncompressedBytes(&pubKey))
+
+	isValid, err := signature.ValidateSECP256R1Signature(hexDigest, hexSign, hexPubKey)
+	assert.NoError(t, err)
+	assert.True(t, isValid)
+
+	// Verify with non hex encoded values
+	isValid = ecdsa.Verify(&pubKey, hash[:], r, s)
+	assert.True(t, isValid)
 }
