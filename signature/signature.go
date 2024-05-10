ccff579eeae9605439f45538bbe5dd09c529e67f
Lorenz Herzberger <64837895+LaurentMontBlanc@users.noreply.github.com>
2024-05-10 09:51:08 +0200

    feat: add secp256r1 signature verification (#14)
    * feat: add secp256r1 signature verification
    * docs: add a comment to ValidateSECP256R1Signature
    * aligned functionality with trust-anchor-wallet
    * adjusted test cases
    
    Signed-off-by: Lorenz Herzberger <lorenzherzberger@gmail.com>
    Signed-off-by: Jürgen Eckel <juergen@riddleandcode.com>
    Co-authored-by: Jürgen Eckel <juergen@riddleandcode.com>

--- signature/signature.go
+++ signature/signature.go
@@ -1,26 +1,25 @@
 package signature
 
 import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/sha256"
+	"encoding/asn1"
 	"encoding/hex"
 	"errors"
+	"math/big"
 
 	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
 )
 
+type ECDSASignature struct {
+	R, S *big.Int
+}
+
 func ValidateSignature(hexMessage string, hexSignature string, hexPublicKey string) (bool, error) {
-	// Convert the message, signature, and public key from hex to bytes
-	messageBytes, err := hex.DecodeString(hexMessage)
+	messageBytes, signatureBytes, publicKeyBytes, err := decodeInputs(hexMessage, hexSignature, hexPublicKey)
 	if err != nil {
-		return false, errors.New("invalid message hex string")
-	}
-	// Convert  signature, and public key from hex to bytes
-	signatureBytes, err := hex.DecodeString(hexSignature)
-	if err != nil {
-		return false, errors.New("invalid signature hex string")
-	}
-	publicKeyBytes, err := hex.DecodeString(hexPublicKey)
-	if err != nil {
-		return false, errors.New("invalid public key hex string")
+		return false, err
 	}
 
 	// Create a secp256k1 public key object
@@ -32,4 +31,83 @@ func ValidateSignature(hexMessage string, hexSignature string, hexPublicKey stri
 		return false, errors.New("invalid signature")
 	}
 	return isValid, nil
+}
+
+// ValidateSECP256R1Signature takes the hex-encoded message together with r, s of the signature (combined as []byte{r.Bytes()..., s.Bytes()...})
+// and x, y (combined as []byte{0x04, x.Bytes()..., y.Bytes()...}) of the public key
+func ValidateSECP256R1Signature(hexMessage string, hexSignature string, hexPublicKey string) (bool, error) {
+	messageBytes, sigBytes, publicKeyBytes, err := decodeInputs(hexMessage, hexSignature, hexPublicKey)
+	if err != nil {
+		return false, err
+	}
+
+	pubKey, err := UncompressedBytesToPublicKey(publicKeyBytes)
+	if err != nil {
+		return false, err
+	}
+
+	// Assuming asn1Data contains an ECDSA signature in DER format
+	var signature ECDSASignature
+	_, err = asn1.Unmarshal(sigBytes, &signature)
+	if err != nil {
+		return false, errors.New("unable to unmarshal signature")
+	}
+
+	hash := sha256.Sum256(messageBytes)
+	isValid := ecdsa.Verify(pubKey, hash[:], signature.R, signature.S)
+	if !isValid {
+		return false, errors.New("invalid signature")
+	}
+	return isValid, nil
+}
+
+func UncompressedBytesToPublicKey(b []byte) (*ecdsa.PublicKey, error) {
+	offset := 1
+	if b[0] != 0x04 && len(b) == 64 {
+		offset = 0
+	}
+
+	curve := elliptic.P256()
+	x := new(big.Int).SetBytes(b[offset : offset+curve.Params().BitSize/8])
+	y := new(big.Int).SetBytes(b[offset+curve.Params().BitSize/8:])
+
+	return &ecdsa.PublicKey{
+		Curve: curve,
+		X:     x,
+		Y:     y,
+	}, nil
+}
+
+func PublicKeyToUncompressedBytes(pub *ecdsa.PublicKey) []byte {
+	xBytes := pub.X.Bytes()
+	yBytes := pub.Y.Bytes()
+
+	// Ensure the bytes are the correct length by adding padding if necessary
+	curveBytes := (pub.Curve.Params().BitSize + 7) / 8 // Rounded up byte length
+	if len(xBytes) < curveBytes {
+		xBytes = append(make([]byte, curveBytes-len(xBytes)), xBytes...)
+	}
+	if len(yBytes) < curveBytes {
+		yBytes = append(make([]byte, curveBytes-len(yBytes)), yBytes...)
+	}
+
+	return append([]byte{0x04}, append(xBytes, yBytes...)...) // 0x04 for uncompressed
+}
+
+func decodeInputs(hexMessage string, hexSignature string, hexPublicKey string) (msgBytes []byte, signBytes []byte, pubBytes []byte, err error) {
+	// Convert the message, signature, and public key from hex to bytes
+	msgBytes, err = hex.DecodeString(hexMessage)
+	if err != nil {
+		return nil, nil, nil, errors.New("invalid message hex string")
+	}
+	// Convert  signature, and public key from hex to bytes
+	signBytes, err = hex.DecodeString(hexSignature)
+	if err != nil {
+		return nil, nil, nil, errors.New("invalid signature hex string")
+	}
+	pubBytes, err = hex.DecodeString(hexPublicKey)
+	if err != nil {
+		return nil, nil, nil, errors.New("invalid public key hex string")
+	}
+	return
 }
--- signature/signature_test.go
+++ signature/signature_test.go
@@ -1,7 +1,14 @@
 package signature_test
 
 import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/asn1"
 	"encoding/hex"
+	"fmt"
+	"math/big"
 	"testing"
 
 	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
@@ -24,4 +31,90 @@ func TestValidateSignature(t *testing.T) {
 	valid, err := signature.ValidateSignature(hexMsg, hexSign, hexPublicKey)
 	assert.True(t, valid)
 	assert.NoError(t, err)
+}
+
+func TestValidateSignaturePreset(t *testing.T) {
+	pubkey := "03A772662A2FED007077DA4751455369C57168264A99932AA0190E02929A18863F"
+	sig := "1F54A44CA6A60C8DDF721900DF96AEDCD8E13301DA8D03138679AEBA2C29501A10571731E405AF5BEE265E2BDD06CF9D44155246ED5521EA3BA5BDDA5ACB9850"
+
+	valid, err := signature.ValidateSignature(pubkey, sig, pubkey)
+	assert.True(t, valid)
+	assert.NoError(t, err)
+}
+
+func TestValidateSECP256R1Signature(t *testing.T) {
+	publicKeyString := "2f7cc9a2f286a2aa249bf441feb594872f43ead0a1f65a710aacad831b55eb3b8d816c7f2f6e9964222cbd3d9147999692527595b74064d62aed3d1b8a3b32be"
+	curve := elliptic.P256()
+	prvKey, err := ecdsa.GenerateKey(curve, rand.Reader)
+	assert.NoError(t, err)
+
+	pubKey := prvKey.PublicKey
+
+	// Convert the message, signature, and public key from hex to bytes
+	publicKeyBytes, err := hex.DecodeString(publicKeyString)
+	assert.NoError(t, err)
+
+	digest := publicKeyBytes
+	hash := sha256.Sum256(digest)
+	r, s, err := ecdsa.Sign(rand.Reader, prvKey, hash[:])
+	assert.NoError(t, err)
+
+	sig := signature.ECDSASignature{R: r, S: s}
+
+	// Encode the struct to ASN.1 DER format
+	asn1Bytes, err := asn1.Marshal(sig)
+	assert.NoError(t, err)
+
+	hexSign := hex.EncodeToString(asn1Bytes)
+	hexPubKey := hex.EncodeToString(signature.PublicKeyToUncompressedBytes(&pubKey))
+
+	isValid, err := signature.ValidateSECP256R1Signature(publicKeyString, hexSign, hexPubKey)
+	assert.NoError(t, err)
+	assert.True(t, isValid)
+
+	// Verify with non hex encoded values
+	isValid = ecdsa.Verify(&pubKey, hash[:], r, s)
+	assert.True(t, isValid)
+}
+
+func TestPublicKeyImport(_ *testing.T) {
+	publicKeyString := "2f7cc9a2f286a2aa249bf441feb594872f43ead0a1f65a710aacad831b55eb3b8d816c7f2f6e9964222cbd3d9147999692527595b74064d62aed3d1b8a3b32be"
+
+	// Decode the hex-encoded string into bytes
+	publicKeyBytes, err := hex.DecodeString(publicKeyString)
+	if err != nil {
+		fmt.Println("Error decoding public key string:", err)
+		return
+	}
+
+	// Extract X and Y coordinates from the byte slice
+	x := new(big.Int).SetBytes(publicKeyBytes[0:32]) // Skip the prefix byte (0x04)
+	y := new(big.Int).SetBytes(publicKeyBytes[32:])
+
+	// Create the ECDSA public key
+	publicKey := ecdsa.PublicKey{
+		Curve: elliptic.P256(),
+		X:     x,
+		Y:     y,
+	}
+	print(publicKey.X)
+}
+
+func TestHashingOfPubKey(t *testing.T) {
+	refPubkey := "c59ee9fda66013cd2b427aff34a9203a934e6841aa87e242dc995313c72e42922d3dfb5f9fdf5871500c101335271e40035805703749c4f6da3b3e688480cc58"
+	refHash := "b33a84c7de03986194d34dd8bd3b9ec7d4dd73aafb1010386c23e2625072e2b3"
+	refHashBytes, err := hex.DecodeString(refHash)
+	assert.NoError(t, err)
+	digest := []byte(refPubkey)
+	hash := sha256.Sum256(digest)
+	assert.Equal(t, [32]uint8(refHashBytes), hash)
+}
+
+func TestValidateSECP256R1SignaturePreset(t *testing.T) {
+	refPubkey := "66ea9383bb65c6e70ccfd3e920f426d4b3f6862fd8098e5f92cbf6ebdb360903f70933548f3345f4e8eb9bb7afb367575ac4e8e9c8a0b44b0f8480a1b344fb88"
+	refSignatureAsn1 := "3045022100affece81a6f53445dc2b61719f1f23ab4c53191f9562b13aa95d612a9ed8816d02201a5af2cceaae4364dce5acbb9b9869a6728a228de9e96043a0b240f6e6a86a0a"
+
+	isValid, err := signature.ValidateSECP256R1Signature(refPubkey, refSignatureAsn1, refPubkey)
+	assert.NoError(t, err)
+	assert.Equal(t, true, isValid)
 }
